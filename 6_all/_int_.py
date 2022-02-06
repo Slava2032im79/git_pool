a1a947499f31a57ef9ebda263d850254290b775f
slava <p.slava79@gmail.com>
2022-02-06 11:13:58 +0300

    second

--- 6_all/_int_.py
+++ 6_all/_int_.py
@@ -1,9 +1,19 @@
-def is_prime (x):
-    b = True
-    for i in range(2, int(x ** 0)):
-        if x % i ==0:
-            b = False
-    return (b)
-a = int(input('введите число '))
-print((is_prime(a)))
+# def is_prime (x):
+#     b = True
+#     for i in range(2, int(x ** 0)):
+#         if x % i ==0:
+#             b = False
+#     return (b)
+# a = int(input('введите число '))
+# print((is_prime(a)))
 
+a = int(input('введите сторону квадрата '))
+def square(a):
+    p = 4 * a
+    s = a * a
+    d = (a ** 2) / 2
+    d = d ** 0.5
+
+    k = (p, s, d)
+
+    return k
